eca7cc4f45d0b09e1f367799c45d64b76dbee5fc
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-04-21 16:38:05 -0600

    Primer avance de la landing page

--- Front/Experience/World/Room.js
+++ Front/Experience/World/Room.js
@@ -13,7 +13,31 @@ export default class Room {
     }
 
     setModel() {
+        this.actualRoom.rotation.y = Math.PI;
+
+        this.actualRoom.children.forEach((child) => {
+            child.castShadow = true;
+            child.receiveShadow = true;
+
+            if(child instanceof THREE.Group) {
+                child.children.forEach((groupchild) => {
+                    groupchild.castShadow = true;
+                    groupchild.receiveShadow = true;
+                })
+            }
+
+            if (child.name === "AquaGlass") {
+                child.material = new THREE.MeshPhysicalMaterial();
+                child.material.roughness = 0;
+                child.material.color.set(0xffffff);
+                child.material.ior = 3;
+                child.material.transmission = 1;
+                child.material.opacity = 1;
+            }
+        });
+
         this.scene.add(this.actualRoom);
+        this.actualRoom.scale.set(0.11, 0.11, 0.11);
     }
 
     resize() {
