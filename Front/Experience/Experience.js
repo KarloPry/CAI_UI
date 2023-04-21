7661de41e56f2001a6b789b73b222f2e2525428f
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-04-21 17:52:07 -0600

    Video en pantalla implementado

--- Front/Experience/Experience.js
+++ Front/Experience/Experience.js
@@ -37,11 +37,13 @@ export default class Experience{
 
     resize() {
         this.camera.resize();
+        this.world.update();
         this.renderer.resize();
     }
     
     update() {
         this.camera.update();
+        this.world.update();
         this.renderer.update();
     }
 }
--- Front/Experience/World/Room.js
+++ Front/Experience/World/Room.js
@@ -6,10 +6,12 @@ export default class Room {
         this.experience = new Experience();
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
+        this.time = this.experience.time;
         this.room = this.resources.items.room;
         this.actualRoom = this.room.scene;
         
         this.setModel();
+        this.setAnimation();
     }
 
     setModel() {
@@ -19,7 +21,6 @@ export default class Room {
 
             if (child instanceof THREE.Group) {
                 child.children.forEach((groupchild) => {
-                    console.log(groupchild.material);
                     groupchild.castShadow = true;
                     groupchild.receiveShadow = true;
                 });
@@ -28,15 +29,28 @@ export default class Room {
             if (child.name === "Aqua") {
                 child.material = new THREE.MeshPhysicalMaterial();
                 child.material.roughness = 0;
-                child.material.color.set(0xffffff);
+                child.material.color.set(0x549dd2);
                 child.material.ior = 3;
                 child.material.transmission = 1;
                 child.material.opacity = 1;
             }
+
+            if (child.name === "Screen") {
+                child.material = new THREE.MeshBasicMaterial({
+                    map: this.resources.items.screen
+                });
+            }
+
         });
 
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.11, 0.11, 0.11);
+    }
+
+    setAnimation() {
+        this.mixer = new THREE.AnimationMixer(this.actualRoom);
+        this.swim = this.mixer.clipAction(this.room.animations[0]);
+        this.swim.play();
     }
 
     resize() {
@@ -44,7 +58,7 @@ export default class Room {
     }
 
     update() {
-
+        this.mixer.update(this.time.delta);
     }
 
 }
